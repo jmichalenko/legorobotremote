fb62ee89100788bc6a600e17513afa06b4bf88d0
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2023-05-05 15:48:15 -0400

    Show Added

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+    basic.showNumber(1)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(3)
+    basic.showNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    basic.showNumber(2)
 })
 radio.setGroup(44)
